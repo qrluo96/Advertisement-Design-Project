b344f4c3a792762cdc72c776569482943a0aa0c7
qrluo96 <qiruiluo2022@u.northwestern.edu>
2019-04-09 00:16:39 -0500

    pretrained model

--- ExtractFeaturesVGG16.py
+++ ExtractFeaturesVGG16.py
@@ -5,10 +5,12 @@ import numpy as np
 
 model = VGG16(weights='imagenet', include_top=False)
 
-img_path = 'elephant.jpg'
+img_path = '../Icon1/Animal/1.png'
 img = image.load_img(img_path, target_size=(224, 224))
 x = image.img_to_array(img)
 x = np.expand_dims(x, axis=0)
 x = preprocess_input(x)
 
 features = model.predict(x)
+# print(features.shape)
+# print(features)
