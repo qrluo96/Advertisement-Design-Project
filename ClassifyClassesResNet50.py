ea0f97488d86e61429de305596f8767fc26fc30c
Qirui Luo <qiruiluo2022@u.northwestern.edu>
2019-04-09 13:38:30 -0500

    Update ClassifyClassesResNet50.py

--- ClassifyClassesResNet50.py
+++ ClassifyClassesResNet50.py
@@ -2,17 +2,23 @@ from keras.applications.resnet50 import ResNet50
 from keras.preprocessing import image
 from keras.applications.resnet50 import preprocess_input, decode_predictions
 import numpy as np
+import os
 
 model = ResNet50(weights='imagenet')
 
-img_path = '../Icon1/Animal/1.png'
-img = image.load_img(img_path, target_size=(224, 224))
-x = image.img_to_array(img)
-x = np.expand_dims(x, axis=0)
+# Read images
+path = '../Icon1Copy/SEO'
+files = os.listdir(path)
+for file in files:
+    img = image.load_img(path+"/"+file, target_size=(224, 224))
+    x = image.img_to_array(img)
+    x = np.expand_dims(x, axis=0)
+
 x = preprocess_input(x)
+#img_path = '../Icon1/Animal/1.png'
 
 preds = model.predict(x)
 # decode the results into a list of tuples (class, description, probability)
 # (one such list for each sample in the batch)
-print('Predicted:', decode_predictions(preds, top=3)[0])
+print('Predicted:', decode_predictions(preds, top=5))
 # Predicted: [(u'n02504013', u'Indian_elephant', 0.82658225), (u'n01871265', u'tusker', 0.1122357), (u'n02504458', u'African_elephant', 0.061040461)]
