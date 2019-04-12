89c96f7713f4af19bf99dab59ab69d966ff0b48f
Qirui Luo <qiruiluo2022@u.northwestern.edu>
2019-04-12 14:18:24 -0500

    Create Preprocess.py

---
+++ Preprocess.py
@@ -0,0 +1,26 @@
+from keras.applications.resnet50 import preprocess_input, decode_predictions
+from keras.preprocessing import image
+import numpy as np
+import os
+
+path = '../Icon1Copy'
+folders = os.listdir(path)
+data = np.zeros((1, 224, 224, 3))
+for folder in folders:
+    if folder[0] == '.':
+        continue
+    files = os.listdir(path+'/'+folder)
+    for file in files:
+        if os.path.splitext(file)[-1] != ".png":
+            continue
+        img = image.load_img(path+'/'+folder+'/'+file, target_size=(224, 224))
+        x = image.img_to_array(img)
+        x = np.expand_dims(x, axis=0)
+        x = preprocess_input(x)
+        data = np.vstack((data, x))
+
+data = data[1:]
+
+dataFile = open("./data.npy", "w")
+np.save(dataFile, data)
+
