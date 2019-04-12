35d4259fb9495ffb0aba1bf9e4653575240d4578
Qirui Luo <qiruiluo2022@u.northwestern.edu>
2019-04-12 13:37:30 -0500

    Update ExtractFeaturesVGG16.py

--- ExtractFeaturesVGG16.py
+++ ExtractFeaturesVGG16.py
@@ -26,15 +26,15 @@ for folder in folders:
 data = data[1:]
 
 features = model.predict(data)
-# print(features.shape)
-# print(features)
+print(features.shape)
+print(features)
 
-estimator = KMeans(n_clusters=100, max_iter=1000, n_jobs=-1)
-estimator.fit(features)
-label_pred = estimator.labels_
-print(label_pred)
-centroids = estimator.cluster_centers_
-print(centroids)
-inertia = estimator.inertia_
-print(inertia)
+# estimator = KMeans(n_clusters=100, max_iter=1000, n_jobs=-1)
+# estimator.fit(features)
+# label_pred = estimator.labels_
+# print(label_pred)
+# centroids = estimator.cluster_centers_
+# print(centroids)
+# inertia = estimator.inertia_
+# print(inertia)
 
