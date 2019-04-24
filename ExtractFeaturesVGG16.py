5ffdcd59f164403ebaa74809b9d8f1e2e01eee4a
Qirui Luo <qiruiluo2022@u.northwestern.edu>
2019-04-23 22:55:26 -0500

    Update ExtractFeaturesVGG16.py

--- ExtractFeaturesVGG16.py
+++ ExtractFeaturesVGG16.py
@@ -7,31 +7,13 @@ from sklearn.cluster import KMeans
 
 model = VGG16(weights='imagenet', include_top=False)
 
-# path = '../Icon1Copy'
-# folders = os.listdir(path)
-# data = np.zeros((1, 224, 224, 3))
-# for folder in folders:
-#     if folder[0] == '.':
-#         continue
-#     files = os.listdir(path+'/'+folder)
-#     for file in files:
-#         if os.path.splitext(file)[-1] != ".png":
-#             continue
-#         img = image.load_img(path+'/'+folder+'/'+file, target_size=(224, 224))
-#         x = image.img_to_array(img)
-#         x = np.expand_dims(x, axis=0)
-#         x = preprocess_input(x)
-#         data = np.vstack((data, x))
-
-# data = data[1:]
-
 data = np.load("../data.npy")
 
 features = model.predict(data)
 print(features.shape)
 print(features)
 
-
+np.save("../feature.npy", features)
 
 # estimator = KMeans(n_clusters=100, max_iter=1000, n_jobs=-1)
 # estimator.fit(features)
