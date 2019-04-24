fd506e65530c0578186fc4fba9012cba2c190ae5
Qirui Luo <qiruiluo2022@u.northwestern.edu>
2019-04-24 02:06:56 -0500

    Update Clustering.py

--- Clustering.py
+++ Clustering.py
@@ -7,7 +7,7 @@ features = np.load("../feature.npy")
 
 features = np.reshape(features, (features.shape[0], -1))
 
-estimator = MiniBatchKMeans(n_clusters=100, batch_size=50)
+estimator = MiniBatchKMeans(n_clusters=100, batch_size=10)
 # max_iter=1000
 
 print("start fitting")
@@ -15,7 +15,14 @@ estimator.fit(features)
 print("finished")
 label_pred = estimator.labels_
 print(label_pred)
-centroids = estimator.cluster_centers_
-print(centroids)
-inertia = estimator.inertia_
-print(inertia)
+# centroids = estimator.cluster_centers_
+# print(centroids)
+# inertia = estimator.inertia_
+# print(inertia)
+
+indexlist = []
+for i in range(100):
+    index = [j for j in range(len(label_pred)) if label_pred[j] == 0]
+    indexlist.append(index)
+
+print(indexlist[0])
